6cf79b95ae2271ef38b19f3f2bf0e7c9178db1a2
4a6f616f Gracinha <jgcmarins@gmail.com>
2017-05-19 23:54:57 -0300

    basic server done

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+mongoose.connect('mongodb://localhost/expense-manager')
+mongoose.Promise = global.Promise
+
+app.use(bodyParser.json())
+
+app.use('/api', require('./routes/api'))
+
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message })
+})
+
+app.listen(4000, () => {
+  console.log('Server is running on port 4000.')
+})
---
+++ backend/routes/api.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/expenses', (err, req, res, next) => {
+})
+
+router.post('/expenses', (err, req, res, next) => {
+})
+
+router.put('/expenses/:id', (err, req, res, next) => {
+})
+
+router.delete('/expenses/:id', (err, req, res, next) => {
+})
+
+module.exports = router
